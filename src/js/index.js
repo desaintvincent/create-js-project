b6b1c2717764b7ba380f2de5761989f147dd81f1
Thomas <destvincent@hotmail.fr>
2020-11-08 18:09:30 +0100

    feat(scss): add scss lint

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,7 @@
 import add from './add'
 
-let aa = add(1, 1)
-aa++
+const additionResult = add(1, 1)
 
-const heading = document.createElement('h1')
-heading.textContent = `Interesting: ${aa}`
-
-// Append heading node to the DOM
-const app = document.querySelector('#root')
-class Game {
-  constructor () {
-    this.name = 'Violin Charades'
-  }
-}
-const myGame = new Game()
-// Create paragraph node
-const p = document.createElement('p')
-p.textContent = `I like ${myGame.name}.`
-app.append(heading, p)
 setTimeout(() => {
-  console.log('je teste pour vérifier que ma arrow function aura disparu au terme du processus de compilation de webpack.')
+  console.log(`the arrow function should disapear and display ${additionResult}`)
 }, 500)
