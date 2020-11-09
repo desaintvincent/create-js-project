0a1a3ef3e338d900ec71f9a4521de4b3a9dda534
Thomas <destvincent@hotmail.fr>
2020-11-09 22:12:20 +0100

    fix(cd): release chnges to git repository

--- release.config.js
+++ release.config.js
@@ -4,10 +4,9 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/git', {
-      assets: ['package.json', 'README.md'],
-      // eslint-disable-next-line no-template-curly-in-string
-      message: 'chore(release): v${nextRelease.version}',
+      assets: ['package.json', 'CHANGELOG.md'],
     }],
+    '@semantic-release/github',
   ],
   preset: 'angular',
 }
