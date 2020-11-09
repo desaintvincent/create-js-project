12aa6f4ab2999cc41652c95c7689d470f812fe92
Thomas <destvincent@hotmail.fr>
2020-11-09 22:03:20 +0100

    feat(cd): generate changelog

--- release.config.js
+++ release.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  debug: true,
-  dryRun: true,
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    ['@semantic-release/git', {
+      assets: ['package.json', 'README.md'],
+      // eslint-disable-next-line no-template-curly-in-string
+      message: 'chore(release): v${nextRelease.version}',
+    }],
   ],
+  preset: 'angular',
 }
