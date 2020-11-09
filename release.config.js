03093acea231b1ef8e87d84a688912545fe66810
Thomas <destvincent@hotmail.fr>
2020-11-09 22:25:18 +0100

    fix(cd): release README and package.json to git repository

--- release.config.js
+++ release.config.js
@@ -1,10 +1,20 @@
+/* eslint-disable no-template-curly-in-string */
 module.exports = {
+  debug: true,
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
     ['@semantic-release/git', {
-      assets: ['package.json', 'CHANGELOG.md'],
+      assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+      message: 'chore(release): v${nextRelease.version}\n\n${nextRelease.notes}',
     }],
     '@semantic-release/github',
   ],
